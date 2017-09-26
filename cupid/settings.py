a0ec976b51f8f9903f8cafa12cc6f21b51179d26
KangSeyun <dcp.k953@gmail.com>
2017-09-26 11:19:21 +0900

    feat(rest): rest framework 추가
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- cupid/settings.py
+++ cupid/settings.py
@@ -25,8 +25,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- cupid/urls.py
+++ cupid/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from api.views.auth import login, join
 
 urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', login),
     url(r'^join/$', join)
