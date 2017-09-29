8a3a641772dad1c801ec816271c6c37a4ac19c52
KangSeyun <dcp.k953@gmail.com>
2017-09-30 03:10:26 +0900

    refact(url): url name 규칙 변경
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- cupid/urls.py
+++ cupid/urls.py
@@ -2,15 +2,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from api.views.auth import login, join
-from api.views.ad import ad, ad_detail, ad_write
+from api.views.ad import ad, ad_detail, ad_write, ad_count
 
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^login/$', login),
-    url(r'^join/$', join),
-    url(r'^ad/$', ad),
-    url(r'^ad/write/$', ad_write),
-    url(r'^ad/(?P<id>\d+)/$', ad_detail),    
+    url(r'^api/v1/auth/admin/', admin.site.urls),
+    url(r'^api/v1/auth/login/$', login),
+    url(r'^api/v1/auth/join/$', join),
+    url(r'^api/v1/ad/$', ad),
+    url(r'^api/v1/ad/write/$', ad_write),
+    url(r'^api/v1/ad/(?P<id>\d+)/$', ad_detail),   
+    url(r'^api/v1/ad/count/$', ad_count),   
 ]
