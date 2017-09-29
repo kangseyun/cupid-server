9483b0dc87063c410c2cf6b878d0e0c2f1fa2956
KangSeyun <dcp.k953@gmail.com>
2017-09-30 03:33:34 +0900

    refact(url): logout insert
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- cupid/urls.py
+++ cupid/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from api.views.auth import login, join
+from api.views.auth import login, join, logout
 from api.views.ad import ad, ad_detail, ad_write, ad_count
 
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/v1/auth/admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^api/v1/auth/login/$', login),
     url(r'^api/v1/auth/join/$', join),
+    url(r'^api/v1/auth/logout/$', logout),
     url(r'^api/v1/ad/$', ad),
     url(r'^api/v1/ad/write/$', ad_write),
     url(r'^api/v1/ad/(?P<id>\d+)/$', ad_detail),   
