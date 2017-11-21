a6435392b0b902a2287c6664317d89e3c51a9e36
KangSeyun <dcp.k953@gmail.com>
2017-11-21 21:11:49 +0900

    레이아웃 정리
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- cupid/urls.py
+++ cupid/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
     url(r'^login/$', login, name='login'),
-    url(r'^api/v1/auth/join/$', join),
+    url(r'^api/v1/auth/join/$', join, name='join'),
     url(r'^api/v1/auth/logout/$', logout, name='logout'),
     url(r'^api/v1/ad/$', ad),
     url(r'^api/v1/ad/write/$', ad_write),
